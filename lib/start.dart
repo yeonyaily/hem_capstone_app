30ec56bce9976cf17e4665bc076ba9210585ab2e
Yxxnsu <dustn0121@naver.com>
2022-02-03 15:24:13 +0900

    [UPDATE] INIT

--- lib/start.dart
+++ lib/start.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
-class StartView extends StatelessWidget {
-  const StartView({Key? key}) : super(key: key);
+class Start extends StatelessWidget {
+  const Start({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Center(
-          child: Text('앱이름 및 로고'),
-        ),
-      ),
+      body: Column(        
+        children: <Widget>[
+          Center(
+            child: SvgPicture.asset(
+              'assets/logo/logo.svg',
+              color: theme.colorScheme.primary,
+              height: 232.w,
+              width: 232.w,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: ()=> null,
+                  child: const Text(
+                    '시작하기',
+                  ),                  
+                ),
+                ElevatedButton(
+                  onPressed: ()=> null,
+                  child: const Text(
+                    '테스트모드',
+                  ),                  
+                ),
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }
