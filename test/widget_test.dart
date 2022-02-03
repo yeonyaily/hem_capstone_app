0b24a83952304518730a23f5c7a63c1f43379c6e
Yxxnsu <dustn0121@naver.com>
2022-02-03 15:24:33 +0900

    [UPDATE] INIT

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hem_capstone_app/main.dart';
+import 'package:hem_capstone_app/public_health_info.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PublicHealthInfo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
