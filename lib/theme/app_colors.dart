fd42abe01a7fc5cf4214e112340b75a5658ce822
Yxxnsu <dustn0121@naver.com>
2022-02-03 15:24:26 +0900

    [UPDATE] INIT

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff15C5BA);
+const secondaryColor = Color(0xff04C584);
+const backgroundColor = Color(0xffFFFFFF);
+const surfaceColor = Color(0xffFFFFFF);
+const errorColor = Color(0xffB00020);
+const onPrimaryColor = Color(0xffFFFFFF);
+
+const disableColor = Color(0xffDBDBDB);
+const basicBlack = Color(0xff0F0F0F);
