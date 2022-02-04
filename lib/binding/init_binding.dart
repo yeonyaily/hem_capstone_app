b0f38376a4f48595da5a62b6ba7528f168ac5df5
Yxxnsu <dustn0121@naver.com>
2022-02-04 15:36:18 +0900

    [UPDATE] sign up controller 추가

--- lib/binding/init_binding.dart
+++ lib/binding/init_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:hem_capstone_app/controllers/auth/auth_controller.dart';
+import 'package:hem_capstone_app/controllers/signup/signup_controller.dart';
 
 class InitBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(SignUpController());
     Get.put(AuthController());
   }
 }
