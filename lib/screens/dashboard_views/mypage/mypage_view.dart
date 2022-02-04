403a061aa1eeb00cf28e28c5a76cc2f87468a54a
Yxxnsu <dustn0121@naver.com>
2022-02-04 15:38:25 +0900

    [ADD] mypage logout 추가

--- lib/screens/dashboard_views/mypage/mypage_view.dart
+++ lib/screens/dashboard_views/mypage/mypage_view.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hem_capstone_app/controllers/controller.dart';
+import 'package:hem_capstone_app/widgets/custom/custom_dialog/custom_dialog.dart';
 
 class MypageView extends StatelessWidget {
   const MypageView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('MyPageView'),
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: ()=> AuthController.to.logout(),
+            icon: Icon(Icons.exit_to_app),
+          )
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: ()=> CustomDialog.showDialog(
+            title: '테스팅',
+            content: '테스트입니다.',
+          ),
+          child: const Text('테스트모드'),
+        ),
+      )
     );
   }
 }
