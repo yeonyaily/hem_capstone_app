a63feaa5731b033cf6ea2312b284b9ca04e1ab3c
Yxxnsu <dustn0121@naver.com>
2022-02-04 15:35:43 +0900

    [ADD] INIT 화면 Splash 대체

--- lib/public_health_info.dart
+++ lib/public_health_info.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:hem_capstone_app/screens/splash.dart';
 
 import 'binding/init_binding.dart';
 import 'routes/app_pages.dart';
-import 'start.dart';
 import 'theme/theme.dart';
 
 class PublicHealthInfo extends StatelessWidget {
@@ -28,7 +28,7 @@ class PublicHealthInfo extends StatelessWidget {
           );
         },
         theme: AppTheme.lightThemeData,
-        home: const Start(),
+        home: const SplashScreen(),
       ),
     );
   }
