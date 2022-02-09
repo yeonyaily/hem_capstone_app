5d7d0f90b6e9e9f24633414ce7b560e6b925802c
Yxxnsu <dustn0121@naver.com>
2022-02-09 11:28:07 +0900

    [ADD] timer controller init

--- lib/binding/signup_binding.dart
+++ lib/binding/signup_binding.dart
@@ -1,11 +1,13 @@
 
 import 'package:get/get.dart';
 import 'package:hem_capstone_app/controllers/signup/signup_controller.dart';
+import 'package:hem_capstone_app/controllers/widgets/timer_controller.dart';
 
 class SignUpBinding extends Bindings{
 
   @override
   void dependencies() {
     Get.put(SignUpController());
+    Get.put(TimerController());
   }
 }
