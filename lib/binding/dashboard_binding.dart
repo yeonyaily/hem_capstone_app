6feb7bb89df2cb1cedfa76bd1b5d679bd3e7e294
Yxxnsu <dustn0121@naver.com>
2022-02-09 19:11:53 +0900

    [ADD] controller

--- lib/binding/dashboard_binding.dart
+++ lib/binding/dashboard_binding.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 import 'package:hem_capstone_app/controllers/dashboard/navigation_controller.dart';
+import 'package:hem_capstone_app/controllers/widgets/radio_controller.dart';
+import 'package:hem_capstone_app/controllers/widgets/switch_controller.dart';
 
 class DashBoardBinding extends Bindings {
   @override
   void dependencies() {
     // Get.put(HomeController());
     Get.put(NavigationController());
+    Get.put(SwitchController());
+    Get.put(RadioController());
   }
 }
