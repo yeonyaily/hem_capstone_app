0a3d272c7efa32fafef88067756ea45182aafc41
Yxxnsu <dustn0121@naver.com>
2022-02-15 14:40:10 +0900

    [UPDATE] 순서 변경

--- lib/binding/init_binding.dart
+++ lib/binding/init_binding.dart
@@ -5,8 +5,8 @@ import 'package:hem_capstone_app/controllers/controller.dart';
 class InitBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(CertController());
     Get.put(SignUpController());
     Get.put(AuthController());
-    Get.put(CertController());
   }
 }
