e3152e97d0ee3a770ceddde18fb701298ce08960
Yxxnsu <dustn0121@naver.com>
2022-02-16 10:32:16 +0900

    [ADD] Empty view

--- lib/screens/dashboard_views/empty_view.dart
+++ lib/screens/dashboard_views/empty_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:hem_capstone_app/constant/constant.dart';
 
 class EmptyPage extends StatelessWidget {
   const EmptyPage({Key? key}) : super(key: key);
@@ -6,9 +9,26 @@ class EmptyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('공동인증서를 등록하셔야 합니다. '),
-      ),
+      body: Center(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                'assets/phi/no_data.svg',
+                width: Get.width * .2,
+                height: Get.height * .4,
+              ),
+              space(height: 64),
+               Text(
+                '공동인증서를 등록해주세요!',
+                style: Theme.of(context).textTheme.headline3,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          )
+        ),
+      )
     );
   }
 }
