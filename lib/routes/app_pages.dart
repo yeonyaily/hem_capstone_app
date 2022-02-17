a4b22d08111c2ce33e5b915904ea31f3978ab623
Yxxnsu <dustn0121@naver.com>
2022-02-17 11:33:39 +0900

    [ADD] Check Detail

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -5,6 +5,7 @@ import 'package:hem_capstone_app/binding/signup_binding.dart';
 import 'package:hem_capstone_app/controllers/controller.dart';
 import 'package:hem_capstone_app/screens/dashboard_views/dashboard.dart';
 import 'package:hem_capstone_app/screens/dashboard_views/home/home.dart';
+import 'package:hem_capstone_app/screens/dashboard_views/physical_check/checkup_detail_screen.dart';
 import 'package:hem_capstone_app/screens/dashboard_views/treat_history/drug_detail_screen.dart';
 import 'package:hem_capstone_app/screens/dashboard_views/treat_history/treat_detail_screen.dart';
 import 'package:hem_capstone_app/screens/login/help_screen.dart';
@@ -27,6 +28,7 @@ class AppPages {
   static const ENTERPWD = Routes.ENTERPWD;
   static const TREATDETAIL = Routes.TREATDETAIL;
   static const DRUGDETAIL = Routes.DRUGDETAIL;
+  static const CHECKUPDETAIL = Routes.CHECKUPDETAIL;
 
   static final routes = [
     GetPage(
@@ -74,6 +76,13 @@ class AppPages {
     GetPage(
       name: DRUGDETAIL, 
       page: ()=> const DrugDetailScreen(),
+    ),
+    GetPage(
+      name: CHECKUPDETAIL, 
+      page: ()=> const CheckUpDetailScreen(),
+      // binding: BindingsBuilder((){
+      //   Get.put(HealthCheckController());
+      // })
     )
   ];
 }
